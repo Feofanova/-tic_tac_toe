deb9b636a532cedd8b9914d5d1ce93c96aabd90b
Dasha Feofanova <darinafeofanova91@gmail.com>
2025-06-09 20:25:38 +0200

    Добавлены исключения

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -1,42 +1,46 @@
 # Объявить клаcc
 from gameparts import Board
-from gameparts.exceptions import FieldIndexError
+from gameparts.exceptions import FieldIndexError, CellOccupiedError
 
 
 def main():
     game = Board()
 # Отрисовать поле в терминале.
+    current_player = 'X'
+    running = True
     game.display()
 
-    while True:
-        try:
-            row = int(input('Введите номер строки:'))
-            if row < 0 or row >= game.field_size:
-                raise FieldIndexError
-            column = int(input('Введите номер столбца:'))
-            if column < 0 or column >= game.field_size:
-                raise FieldIndexError
-        except FieldIndexError:
-            print(
-                'Значение должно быть неотрицательным и меньше '
-                f'{game.field_size}.'
-            )
-            print('Пожалуйста, введите значения для строки и столбца заново.')
-            continue
-        except ValueError:
-            print('Буквы вводить нельзя, только числа.')
-            print('Пожалуйста, введите значения для строки и столбца заново')
-            continue
-        except Exception as e:
-            print(f'Возникла ошибка: {e}')
-        else:
-            break
+    while running:
+        print(f'Ход делают {current_player}')
+        while True:
+            try:
+                row = int(input('Введите номер строки:'))
+                if row < 0 or row >= game.field_size:
+                    raise FieldIndexError
+                column = int(input('Введите номер столбца:'))
+                if column < 0 or column >= game.field_size:
+                    raise FieldIndexError
+            except FieldIndexError:
+                print(
+                    'Значение должно быть неотрицательным и меньше '
+                    f'{game.field_size}.'
+                )
+                print('Пожалуйста, введите значения для строки и столбца заново.')
+                continue
+            except ValueError:
+                print('Буквы вводить нельзя, только числа.')
+                print('Пожалуйста, введите значения для строки и столбца заново')
+                continue
+            except Exception as e:
+                print(f'Возникла ошибка: {e}')
+            else:
+                break
 # Разместить на поле символ по указанным координатам - сделать ход.
-    game.make_move(row, column, 'X')
-    print('Ход сделан!')
+        game.make_move(row, column, current_player)
 # Перерисовать поле с учётом сделанного хода.
-    game.display()
+        game.display()
 # Создать игровое поле - объект класса Board.
+        current_player = '0' if current_player == 'X' else 'X'
 
 if __name__ == '__main__':
     main()
--- tic_tac_toe/gameparts/exceptions.py
+++ tic_tac_toe/gameparts/exceptions.py
@@ -5,3 +5,7 @@ class FieldIndexError(IndexError):
             self, message = 'Введено значение за границами игрового поля!'
             ):
         super().__init__(message)
+
+class CellOccupiedError(Exception):
+    def __init__(self, message = 'Попытка изменить занятую ячейку'):
+        super().__init__(message)
