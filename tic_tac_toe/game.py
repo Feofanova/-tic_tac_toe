d37cc25b3497520694c977a64ab54caa176822e9
Dasha Feofanova <darinafeofanova91@gmail.com>
2025-06-09 20:02:10 +0200

    Первый коммит

---
+++ tic_tac_toe/game.py
@@ -0,0 +1,43 @@
+# Объявить клаcc
+from gameparts import Board
+from gameparts.exceptions import FieldIndexError
+
+
+def main():
+    game = Board()
+# Отрисовать поле в терминале.
+    game.display()
+
+    while True:
+        try:
+            row = int(input('Введите номер строки:'))
+            if row < 0 or row >= game.field_size:
+                raise FieldIndexError
+            column = int(input('Введите номер столбца:'))
+            if column < 0 or column >= game.field_size:
+                raise FieldIndexError
+        except FieldIndexError:
+            print(
+                'Значение должно быть неотрицательным и меньше '
+                f'{game.field_size}.'
+            )
+            print('Пожалуйста, введите значения для строки и столбца заново.')
+            continue
+        except ValueError:
+            print('Буквы вводить нельзя, только числа.')
+            print('Пожалуйста, введите значения для строки и столбца заново')
+            continue
+        except Exception as e:
+            print(f'Возникла ошибка: {e}')
+        else:
+            break
+# Разместить на поле символ по указанным координатам - сделать ход.
+    game.make_move(row, column, 'X')
+    print('Ход сделан!')
+# Перерисовать поле с учётом сделанного хода.
+    game.display()
+# Создать игровое поле - объект класса Board.
+
+if __name__ == '__main__':
+    main()
+    
